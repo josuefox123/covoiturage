abe9b84fe94a43ad298d7dde3e7fa61bea1f4b48
josuefox123 <foxjosue9@gmail.com>
2026-08-05 15:43:13 +0100

    Update root repo with AuthMiddlewareStack in asgi.py

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -29,12 +29,15 @@ django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 from api.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     # Requêtes HTTP classiques → Django
     'http': get_asgi_application(),
 
-    # Connexions WebSocket → ChatConsumer
-    'websocket': URLRouter(websocket_urlpatterns),
+    # Connexions WebSocket → NotificationConsumer / ChatConsumer
+    'websocket': AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
 })
